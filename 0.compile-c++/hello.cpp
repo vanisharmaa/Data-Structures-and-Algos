0281b202e801a93b8185ea125967bba29f2348c1
Vani Sharma <vani.7135@gmail.com>
2025-07-29 09:06:24 +0530

    Add .gitignore and initial C++ program

---
+++ 0.compile-c++/hello.cpp
@@ -0,0 +1,8 @@
+// This is a simple C++ program that prints "Hello, Mac!" to the console.
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Mac!" << endl;
+    return 0;
+}
